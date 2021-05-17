79c020370b5f13700e609b0b33a75d2d07930971
Alex Biddulph <c3124185@uon.edu.au>
2021-05-17 13:44:10 +1000

    Generate correct Hwc matrices

--- controllers/collect_vision_data/collect_vision_data.cpp
+++ controllers/collect_vision_data/collect_vision_data.cpp
@@ -17,19 +17,20 @@
  * Copyright 2021 NUbots <nubots@nubots.net>
  */
 
-#include <fstream>
-#include <iostream>
-#include <random>
-#include <array>
-#include <filesystem>
-#include "yaml-cpp/yaml.h"
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include <sstream>
+#include <array>
+#include <filesystem>
+#include <fstream>
 #include <iomanip>
-
-#include <webots/Supervisor.hpp>
+#include <iostream>
+#include <random>
+#include <sstream>
 #include <webots/Camera.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Supervisor.hpp>
+
+#include "yaml-cpp/yaml.h"
 
 std::string padLeft(int number, int width) {
     std::stringstream ss;
@@ -48,7 +49,7 @@ int main(int argc, char** argv) {
     }
     // Load def argument which will be used to identify the robot using the webots getFromDef function
     std::string def = argv[1];
-    
+
     //---------READ CONFIG-----------//
     // AxisAngle rotations will be read from a config file and saved here
     std::vector<std::array<double, 4>> rotations;
@@ -73,17 +74,14 @@ int main(int argc, char** argv) {
     //---------GET ROBOT, CAMERAS AND SUPERVISOR----------//
     // create the Supervisor instance and assign it to a robot
     webots::Supervisor supervisor = webots::Supervisor();
-    webots::Node& robot          = *supervisor.getFromDef(def);
+    webots::Node* robot           = supervisor.getFromDef(def);
 
     // Get the time step of the current world.
     int time_step = int(supervisor.getBasicTimeStep());
 
-    // Create the Robot instance
-    // std::unique_ptr<webots::Robot> robot = std::make_unique<webots::Robot>();
-
     // Get the cameras
     std::unique_ptr<webots::Camera> right_camera = std::make_unique<webots::Camera>("right_camera");
-    std::unique_ptr<webots::Camera> left_camera = std::make_unique<webots::Camera>("left_camera");
+    std::unique_ptr<webots::Camera> left_camera  = std::make_unique<webots::Camera>("left_camera");
     right_camera->enable(time_step);
     left_camera->enable(time_step);
     right_camera->recognitionEnable(time_step);
@@ -91,14 +89,14 @@ int main(int argc, char** argv) {
     right_camera->enableRecognitionSegmentation();
     left_camera->enableRecognitionSegmentation();
 
-    int camera_width = left_camera->getWidth();
-    int camera_height = left_camera->getHeight();
-    double horizontal_fov = left_camera->getFov();
-    double vertical_fov = 2 * std::atan(std::tan(horizontal_fov * 0.5) * ((double) camera_height / camera_width));
-
-    double focal_length_px = 0.5 * camera_height / std::tan(vertical_fov / 2.0);
-
-    std::cout << "focal_length_px: " << focal_length_px << std::endl;
+    // Calculate camera field of view
+    int camera_width             = left_camera->getWidth();
+    int camera_height            = left_camera->getHeight();
+    const double camera_diagonal = std::sqrt(camera_width * camera_width + camera_height * camera_height);
+    const double horizontal_fov  = left_camera->getFov();
+    const double vertical_fov = 2 * std::atan(std::tan(horizontal_fov * 0.5) * (double(camera_height) / camera_width));
+    const double focal_length_px = 0.5 * camera_height / std::tan(vertical_fov / 2.0);
+    const double diagonal_fov    = 2 * std::atan(camera_diagonal / (2 * focal_length_px));
 
     // Create directories for saving data
     std::filesystem::create_directories("./data/data_mono");
@@ -111,14 +109,15 @@ int main(int argc, char** argv) {
     std::uniform_int_distribution<> xDistrib(0, 1080);
     std::uniform_int_distribution<> yDistrib(0, 760);
     std::uniform_int_distribution<> zDistrib(-5, 5);
+    std::uniform_real_distribution<> servoDistrib(-M_PI * 0.5, M_PI * 0.5);
 
     //--------MAIN CONTROL LOOP------------//
     while (supervisor.step(time_step) != -1) {
         //----------SET TRANSLATION OF ROBOT------------//
         // Grab the current translation field of the robot to modify
-        webots::Field& robot_translation_field = *robot.getField("translation");
+        webots::Field* robot_translation_field = robot->getField("translation");
         // Convert the field to a vector to output to console
-        const double* robot_translation_vec = robot_translation_field.getSFVec3f();
+        const double* robot_translation_vec = robot_translation_field->getSFVec3f();
 
         // 0, 0, 0 is centre of the playing field.
         // X ranges from -5.4 - 5.4, the positive value on the left hand side when looking
@@ -128,101 +127,93 @@ int main(int argc, char** argv) {
         // If the robot teleports into an existing object it may run into issues for that
         // image only, after resetPhysics is should return to a regular state
         std::array<double, 3> newPos;
-        newPos[0] = 0.0; // 5.4 - xDistrib(gen) / 100;
-        newPos[1] = 0.0; // 3.8 - yDistrib(gen) / 100;
-        newPos[2] = 0.51; // - zDistrib(gen) / 100;
+        newPos[0] = 0.0;   // 5.4 - xDistrib(gen) / 100;
+        newPos[1] = 0.0;   // 3.8 - yDistrib(gen) / 100;
+        newPos[2] = 0.51;  // - zDistrib(gen) / 100;
 
         // Set new location
-        robot_translation_field.setSFVec3f(newPos.data());
+        robot_translation_field->setSFVec3f(newPos.data());
         supervisor.step(time_step);
 
-        // const double* values = robot_translation_field.getSFVec3f();
-        // std::cout << values[0] << ", " << values[1] << ", " << values[2] << std::endl;
-        // std::cout << newPos[0] << ", " << newPos[1] << ", " << newPos[2] << std::endl;
-
         // Grab the current rotation field of the robot to modify
-        webots::Field& robot_rotation_field = *(robot.getField("rotation"));
+        webots::Field* robot_rotation_field = robot->getField("rotation");
         // Convert the field to a vector to output to console
-        const double* robot_rotation_vec = robot_rotation_field.getSFRotation();
+        const double* robot_rotation_vec = robot_rotation_field->getSFRotation();
 
         // ---------LOOP OVER ROTATIONS--------//
         for (const std::array<double, 4>& rotation : rotations) {
-            const std::array<double, 4> rotationX = rotation; // { 1.000000, 0.000000, 0.0, 0 };
+            const std::array<double, 4> rotationX = rotation;  // { 1.000000, 0.000000, 0.0, 0 };
 
             //-----------SET ROTATION----------//
             // Prepare new rotation. These are saved in rotations vector as the axis-angle
             // calculation is rough to calculate on the fly
             // Apply new rotation and reset physics to avoid robot tearing itself apart
-            robot_rotation_field.setSFRotation(rotationX.data());  // Rotation is interpreted as [rx, ry, rz, \alpha]
-            robot.resetPhysics();
+            robot_rotation_field->setSFRotation(rotationX.data());  // Rotation is interpreted as [rx, ry, rz, \alpha]
+            robot->resetPhysics();
+            supervisor.step(time_step);
 
-            // TODO(KipHamiltons) verify this is the right rotation - as in, verify is Roc, not Rco, and that the encoding of xyzw is the same order here
-            // Get WORLD TO TORSO
-            // World to torso transformation
-            Eigen::Affine3d Htw;
-            Htw.linear() = Eigen::AngleAxisd(rotationX[3], Eigen::Vector3d(rotationX[0], rotationX[1], rotationX[2])).toRotationMatrix();
-            Htw.translation() = Htw.rotation() * -Eigen::Vector3d(newPos.data());
+            /**************************************************************
+             * From the NUbots repo                                       *
+             * File: ForwardKinematics.hpp                                *
+             * Function: calculateHeadJointPosition                       *
+             **************************************************************/
+            Eigen::Affine3d Htx = Eigen::Affine3d::Identity();
 
-            std::cout << "Htw " << std::endl << Htw.matrix() << std::endl << std::endl;
+            // From Kinematics Configuration
+            const Eigen::Vector3d NECK_POS(-0.007, 0.0, 0.21);
+            const double NECK_LENGTH = 0.048;
 
-            // Get TORSO TO NECK
-            // relative to torso, torso to neck
-            const double* rNTt = robot.getFromProtoDef("neck_solid")->getField("translation")->getSFVec3f();
-            // Rtn, torso to neck - Head Yaw?
-            const double* Rnt = robot.getFromProtoDef("neck_solid")->getField("rotation")->getSFRotation();  // Rotation is interpreted as [rx, ry, rz, \alpha]
+            // Translate to base of neck from origin
+            Htx = Htx.translate(NECK_POS);
+            // Rotate to face out of base of neck
+            Htx = Htx.rotate(Eigen::AngleAxisd(-M_PI_2, Eigen::Vector3d::UnitY()));
+            // Rotate head in yaw axis
+            Htx = Htx.rotate(Eigen::AngleAxisd(robot->getFromProtoDef("neck_yaw")->getField("position")->getSFFloat(),
+                                               Eigen::Vector3d::UnitX()));
+            // Translate to top of neck (i.e. next motor axle)
+            Htx = Htx.translate(Eigen::Vector3d(NECK_LENGTH, 0.0, 0.0));
 
-            // Homogenous transformation of the neck to the robot's torso - Matrix from torso to head yaw
-            Eigen::Affine3d Hnt;
-            Hnt.linear() = Eigen::AngleAxisd(Rnt[3], Eigen::Vector3d(Rnt[0], Rnt[1], Rnt[2])).toRotationMatrix();
-            Hnt.translation() = Hnt.rotation() * -Eigen::Vector3d(rNTt[0], rNTt[1], rNTt[2]);
+            // YAW
+            // Return the basis pointing out of the top of the torso with z pointing out the back of the neck. Pos
+            // is top of neck (at hip pitch motor)
 
-            std::cout << "Hnt " << std::endl << Hnt.matrix() << std::endl << std::endl;
+            // Rotate to face forward direction of neck
+            Htx = Htx.rotate(Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d::UnitY()));
+            // Rotate pitch
+            Htx = Htx.rotate(Eigen::AngleAxisd(robot->getFromProtoDef("head_pitch")->getField("position")->getSFFloat(),
+                                               Eigen::Vector3d::UnitY()));
+            // PITCH
+            // Return basis pointing along camera vector (ie x is camera vector, z out of top of head). Pos at
+            // camera position
 
+            /**************************************************************
+             * From the NUbots repo                                       *
+             * Module: input/Camera                                       *
+             * Files: Left.yaml and Right.yaml                            *
+             **************************************************************/
+            // clang-format off
+            Eigen::Affine3d Hxl;
+            Hxl.matrix() << 0.9997403694613228,   -0.016551656784825577, -0.01566002262642871,  0.08893612063588988,
+                            0.01728764296000275,   0.9986932814319833,    0.04809227613146373,  0.03529909622045881,
+                            0.014843552535558163, -0.04835051478781678,   0.9987201292902445,   0.0713674563254241,
+                            0.0,                   0.0,                   0.0,                  1.0;
+            Eigen::Affine3d Hxr;
+            Hxr.matrix() << 0.9997403694613228,   -0.016551656784825577, -0.01566002262642871,  0.08893612063588988,
+                            0.01728764296000275,   0.9986932814319833,    0.04809227613146373, -0.03529909622045881,
+                            0.014843552535558163, -0.04835051478781678,   0.9987201292902445,   0.0713674563254241,
+                            0.0,                   0.0,                   0.0,                  1.0;
+            // clang-format on
 
-            // Get NECK TO TRANSFORM - Head yaw to head pitch
-            // relative to torso, torso to neck
-            const double* rXNn = robot.getFromProtoDef("neck_transform")->getField("translation")->getSFVec3f();
-            // Rtn, torso to neck
-            const double* Rxn = robot.getFromProtoDef("neck_transform")->getField("rotation")->getSFRotation();  // Rotation is interpreted as [rx, ry, rz, \alpha]
+            // Htw
+            Eigen::Affine3d Htw = Eigen::Affine3d::Identity();
+            Htw.linear() = Eigen::AngleAxisd(rotationX[3], Eigen::Vector3d(rotationX[0], rotationX[1], rotationX[2]))
+                               .toRotationMatrix()
+                               .transpose();
+            Htw.translation() = -Htw.linear() * Eigen::Vector3d(newPos[0], newPos[1], newPos[2]);
 
-            // Homogenous transformation of the neck to the robot's torso - Matrix from head yaw to head pitch
-            Eigen::Affine3d Hxn;
-            Hxn.linear() = Eigen::AngleAxisd(Rxn[3], Eigen::Vector3d(Rxn[0], Rxn[1], Rxn[2])).toRotationMatrix();
-            Hxn.translation() = Hxn.rotation() * -Eigen::Vector3d(rXNn[0], rXNn[1], rXNn[2]);
-
-            std::cout << "Hxn " << std::endl << Hxn.matrix() << std::endl << std::endl;
-
-            // Get TRANSFORM TO CAMERA
-            // const double* rCXn = robot.getFromProtoDef("right_camera")->getField("translation")->getSFVec3f();
-            // const double* Rcx = robot.getFromProtoDef("right_camera")->getField("rotation")->getSFRotation();  // Rotation is interpreted as [rx, ry, rz, \alpha]
-            
-            Eigen::Affine3d Hcx;
-
-            Hcx.linear() = Eigen::AngleAxisd(2* M_PI / 180, Eigen::Vector3d(0,1,0)).toRotationMatrix();
-            Hcx.translation() = Hcx.rotation() * -Eigen::Vector3d(0.069952, 0.033795, 0.06488);
-
-            std::cout << "Hcx " << std::endl << Hcx.matrix() << std::endl << std::endl;
-
-            // Hcx.linear() = Eigen::AngleAxisd(Rcx[3], Eigen::Vector3d(Rcx[0], Rcx[1], Rcx[2])).toRotationMatrix();
-            // Hcx.translation() = Hcx.rotation() * -Eigen::Vector3d(rCXn[0], rCXn[1], rCXn[2]);
-
-            std::cout << "(Hnt * Htw) " << std::endl << (Hnt * Htw).matrix() << std::endl << std::endl;
-            std::cout << "(Hxn * (Hnt * Htw)) " << std::endl << (Hxn * (Hnt * Htw)).matrix() << std::endl << std::endl;
-            std::cout << "(Hcx * (Hxn * (Hnt * Htw))) " << std::endl << (Hcx * (Hxn * (Hnt * Htw))).matrix() << std::endl << std::endl;
-
-            Eigen::Affine3d Hcw = (Hcx * (Hxn * (Hnt * Htw)));
-            Eigen::Affine3d Hwc = Hcw.inverse();
-
-            // This fixes roll being inverted, but we're not sure why
-            // Hwc.linear() = Hwc.rotation().transpose();
-
-
-            std::cout << "Hcw " << std::endl << Hcw.matrix() << std::endl << std::endl;
-            std::cout << "Hwc " << std::endl << Hwc.matrix() << std::endl << std::endl;
-
-            // Hoc.linear() = Roc.matrix();
-            // // TODO(YsobelSims) should this vector be negated?? this might be rWCc or rCWc or some other thing kip didn't think of
-            // Hoc.translation() = Eigen::Vector3d(newPos.data());
+            // Calculate the left and right camera to world transformation matrices
+            Eigen::Affine3d Hwc_l = Htw.inverse() * Htx * Hxl;
+            Eigen::Affine3d Hwc_r = Htw.inverse() * Htx * Hxr;
 
             //-----------SAVE DATA-----------//
             std::string count_padded = padLeft(count, 7);
@@ -230,12 +221,15 @@ int main(int argc, char** argv) {
             // Save stereo images
             left_camera->saveImage("./data/data_stereo/image" + count_padded + "_L.jpg", QUALITY);
             right_camera->saveImage("./data/data_stereo/image" + count_padded + "_R.jpg", QUALITY);
-            left_camera->saveRecognitionSegmentationImage("./data/data_stereo/image" + count_padded + "_L_mask.png", QUALITY);
-            right_camera->saveRecognitionSegmentationImage("./data/data_stereo/image" + count_padded + "_R_mask.png", QUALITY);
+            left_camera->saveRecognitionSegmentationImage("./data/data_stereo/image" + count_padded + "_L_mask.png",
+                                                          QUALITY);
+            right_camera->saveRecognitionSegmentationImage("./data/data_stereo/image" + count_padded + "_R_mask.png",
+                                                           QUALITY);
 
             // Save mono images
             left_camera->saveImage("./data/data_mono/image" + count_padded + ".jpg", QUALITY);
-            left_camera->saveRecognitionSegmentationImage("./data/data_mono/image" + count_padded + "_mask.png", QUALITY);
+            left_camera->saveRecognitionSegmentationImage("./data/data_mono/image" + count_padded + "_mask.png",
+                                                          QUALITY);
 
             // For some reason the first iteration has the robot in the wrong position
             if (count == 0) {
@@ -250,13 +244,13 @@ int main(int argc, char** argv) {
             lensYaml << YAML::Key << "focal_length" << YAML::Value << focal_length_px;
             lensYaml << YAML::Key << "centre" << YAML::Flow << YAML::BeginSeq << 0 << 0 << YAML::EndSeq;
             lensYaml << YAML::Key << "k" << YAML::Flow << YAML::BeginSeq << 0 << 0 << YAML::EndSeq;
-            lensYaml << YAML::Key << "fov" << YAML::Value << left_camera->getFov();
+            lensYaml << YAML::Key << "fov" << YAML::Value << diagonal_fov;
             lensYaml << YAML::Key << "Hoc" << YAML::Value;
             lensYaml << YAML::BeginSeq;
-            lensYaml << YAML::Flow << std::vector({Hwc(0, 0), Hwc(0, 1), Hwc(0, 2), Hwc(0,3)});
-            lensYaml << YAML::Flow << std::vector({Hwc(1, 0), Hwc(1, 1), Hwc(1, 2), Hwc(1,3)});
-            lensYaml << YAML::Flow << std::vector({Hwc(2, 0), Hwc(2, 1), Hwc(2, 2), Hwc(2,3)});
-            lensYaml << YAML::Flow << YAML::BeginSeq << 0 << 0 << 0 << 1 << YAML::EndSeq;
+            lensYaml << YAML::Flow << std::vector({Hwc_l(0, 0), Hwc_l(0, 1), Hwc_l(0, 2), Hwc_l(0, 3)});
+            lensYaml << YAML::Flow << std::vector({Hwc_l(1, 0), Hwc_l(1, 1), Hwc_l(1, 2), Hwc_l(1, 3)});
+            lensYaml << YAML::Flow << std::vector({Hwc_l(2, 0), Hwc_l(2, 1), Hwc_l(2, 2), Hwc_l(2, 3)});
+            lensYaml << YAML::Flow << std::vector({Hwc_l(3, 0), Hwc_l(3, 1), Hwc_l(3, 2), Hwc_l(3, 3)});
             lensYaml << YAML::EndSeq;
             lensYaml << YAML::EndMap;
 
