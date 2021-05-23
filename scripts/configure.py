d38f7170eca3f2eced96bb8ae9b7eb25e12b0d79
Kip Hamiltons <48076495+KipHamiltons@users.noreply.github.com>
2021-05-23 18:52:01 +1000

    Adds --clean option for configure (#50)
    * Adds --clean option for configure
    
    * use b.build_dir because it's defined so we might as well.
    
    * Typo josephus spotted
    
    Co-authored-by: Josephus Paye II <j.paye96@gmail.com>
    
    Co-authored-by: Josephus Paye II <j.paye96@gmail.com>

--- scripts/configure.py
+++ scripts/configure.py
@@ -11,6 +11,14 @@ def register(command):
     command.help = "Configure the project in a docker container"
 
     command.add_argument(
+        "--clean",
+        dest="purge_build_folder",
+        action="store_true",
+        default=False,
+        help="Purge the build folder before configuring",
+    )
+
+    command.add_argument(
         "-i",
         "--interactive",
         dest="interactive",
@@ -21,14 +29,20 @@ def register(command):
     command.add_argument("args", nargs="...", help="the arguments to pass through to cmake")
 
 
-def run(interactive, args, **kwargs):
+def run(purge_build_folder, interactive, args, **kwargs):
 
-    # Make sure we have a build directory
-    os.makedirs("build", exist_ok=True)
+    # Purge the build folder if we have passed the purge_build_folder argument
+    if purge_build_folder:
+        from shutil import rmtree
+
+        # We ignore errors so that it doesn't fail if `build` doesn't exist
+        rmtree(b.build_dir, ignore_errors=True)
+
+    # Make sure we have a build directory, then change into it
+    os.makedirs(b.build_dir, exist_ok=True)
+    os.chdir(b.build_dir)
 
     # If interactive then run ccmake else just run cmake
-    os.chdir(os.path.join(b.project_dir, "build"))
-
     command = ["ccmake" if interactive else "cmake"]
 
     # To pass arguments to the cmake command you put them after "--"
