da29c801ec6b7305e9b0e748b49a2a39f6c6867f
Kip Hamiltons <48076495+KipHamiltons@users.noreply.github.com>
2021-06-02 20:27:17 +1000

    WEBOTS_HOME issue fix (#69)
    * Fixes loop encountered when people don't run source ~/.bashrc before ./b configure
    
    * Adds comment clarifying change
    
    Co-authored-by: Y0sh1dk <47013245+Y0sh1dk@users.noreply.github.com>

--- cmake/Modules/Findwebots.cmake
+++ cmake/Modules/Findwebots.cmake
@@ -1,7 +1,9 @@
 if(NOT webots_FOUND)
   # If WEBOTS_HOME is not already set, set it to the value of the WEBOTS_HOME environment variable. If the environment
   # variable is not defined, then choose some sort of sane default
-  if(NOT DEFINED WEBOTS_HOME)
+  # We also re-set WEBOTS_HOME if it's the default value we assign it in the case that it wasn't set previously
+  # which avoids a loop which happens if people run `./b configure` before `source ~/.bashrc` during setup
+  if(NOT DEFINED WEBOTS_HOME OR WEBOTS_HOME STREQUAL "/usr/local/webots")
     if(DEFINED ENV{WEBOTS_HOME})
       set(WEBOTS_HOME
           $ENV{WEBOTS_HOME}
