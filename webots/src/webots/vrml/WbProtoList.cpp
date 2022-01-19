155193e767e620385668ebdf12b672ba13c42f16
Ysobel <ysobelsims@gmail.com>
2022-01-19 15:13:22 +1100

    Remove unecessary formatting

--- webots/src/webots/vrml/WbProtoList.cpp
+++ webots/src/webots/vrml/WbProtoList.cpp
@@ -14,8 +14,6 @@
 
 #include "WbProtoList.hpp"
 
-#include <QtCore/QDir>
-
 #include "WbNode.hpp"
 #include "WbParser.hpp"
 #include "WbPreferences.hpp"
@@ -24,190 +22,186 @@
 #include "WbTokenizer.hpp"
 #include "WbVrmlWriter.hpp"
 
-WbProtoList* gCurrent = NULL;
+#include <QtCore/QDir>
+
+WbProtoList *gCurrent = NULL;
 QFileInfoList WbProtoList::gResourcesProtoCache;
 QFileInfoList WbProtoList::gProjectsProtoCache;
 QFileInfoList WbProtoList::gExtraProtoCache;
 
-WbProtoList* WbProtoList::current() {
-    return gCurrent;
+WbProtoList *WbProtoList::current() {
+  return gCurrent;
 }
 
-WbProtoList::WbProtoList(const QString& primarySearchPath) {
-    gCurrent           = this;
-    mPrimarySearchPath = primarySearchPath;
+WbProtoList::WbProtoList(const QString &primarySearchPath) {
+  gCurrent = this;
+  mPrimarySearchPath = primarySearchPath;
 
-    static bool firstCall = true;
-    if (firstCall) {
-        updateResourcesProtoCache();
-        updateProjectsProtoCache();
-        updateExtraProtoCache();
-        firstCall = false;
-    }
+  static bool firstCall = true;
+  if (firstCall) {
+    updateResourcesProtoCache();
+    updateProjectsProtoCache();
+    updateExtraProtoCache();
+    firstCall = false;
+  }
 
-    updatePrimaryProtoCache();
+  updatePrimaryProtoCache();
 }
 
-// we do not delete the PROTO models here: each PROTO model is automatically deleted when its last PROTO instance is
-// deleted
+// we do not delete the PROTO models here: each PROTO model is automatically deleted when its last PROTO instance is deleted
 WbProtoList::~WbProtoList() {
-    if (gCurrent == this)
-        gCurrent = NULL;
+  if (gCurrent == this)
+    gCurrent = NULL;
 
-    foreach (WbProtoModel* model, mModels)
-        model->unref();
+  foreach (WbProtoModel *model, mModels)
+    model->unref();
 }
 
-void WbProtoList::findProtosRecursively(const QString& dirPath, QFileInfoList& protoList, bool inProtos) {
-    QDir dir(dirPath);
-    if (!dir.exists() || !dir.isReadable())
-        // no PROTO nodes
-        return;
+void WbProtoList::findProtosRecursively(const QString &dirPath, QFileInfoList &protoList, bool inProtos) {
+  QDir dir(dirPath);
+  if (!dir.exists() || !dir.isReadable())
+    // no PROTO nodes
+    return;
 
-    // search in folder
-    if (inProtos) {
-        QStringList filter("*.proto");
-        protoList.append(dir.entryInfoList(filter, QDir::Files, QDir::Name));
-    }
-    // search in subfolders
-    QFileInfoList subfolderInfoList = dir.entryInfoList(QDir::AllDirs | QDir::NoDotAndDotDot);
+  // search in folder
+  if (inProtos) {
+    QStringList filter("*.proto");
+    protoList.append(dir.entryInfoList(filter, QDir::Files, QDir::Name));
+  }
+  // search in subfolders
+  QFileInfoList subfolderInfoList = dir.entryInfoList(QDir::AllDirs | QDir::NoDotAndDotDot);
 
-    if (!inProtos) {
-        // try to identify a project root folder
-        foreach (QFileInfo subfolder, subfolderInfoList) {
-            const QString& fileName = subfolder.fileName();
-            if (fileName == "controllers" || fileName == "worlds" || fileName == "protos" || fileName == "plugins") {
-                const QString protosPath = dirPath + "/protos";
-                if (QFile::exists(protosPath))
-                    findProtosRecursively(protosPath, protoList, true);
-                return;
-            }
-        }
-    }
+  if (!inProtos) {
+    // try to identify a project root folder
     foreach (QFileInfo subfolder, subfolderInfoList) {
-        if (inProtos
-            && (subfolder.fileName() == "textures" || subfolder.fileName() == "icons"
-                || subfolder.fileName() == "meshes")) {
-            // skip any textures or icons subfolder inside a protos folder
-            continue;
-        }
-        findProtosRecursively(subfolder.absoluteFilePath(), protoList, inProtos);
+      const QString &fileName = subfolder.fileName();
+      if (fileName == "controllers" || fileName == "worlds" || fileName == "protos" || fileName == "plugins") {
+        const QString protosPath = dirPath + "/protos";
+        if (QFile::exists(protosPath))
+          findProtosRecursively(protosPath, protoList, true);
+        return;
+      }
     }
+  }
+  foreach (QFileInfo subfolder, subfolderInfoList) {
+    if (inProtos &&
+        (subfolder.fileName() == "textures" || subfolder.fileName() == "icons" || subfolder.fileName() == "meshes")) {
+      // skip any textures or icons subfolder inside a protos folder
+      continue;
+    }
+    findProtosRecursively(subfolder.absoluteFilePath(), protoList, inProtos);
+  }
 }
 
 void WbProtoList::updateResourcesProtoCache() {
-    gResourcesProtoCache.clear();
-    QFileInfoList protosInfo;
-    findProtosRecursively(WbStandardPaths::resourcesProjectsPath(), protosInfo);
-    gResourcesProtoCache << protosInfo;
+  gResourcesProtoCache.clear();
+  QFileInfoList protosInfo;
+  findProtosRecursively(WbStandardPaths::resourcesProjectsPath(), protosInfo);
+  gResourcesProtoCache << protosInfo;
 }
 
 void WbProtoList::updateProjectsProtoCache() {
-    gProjectsProtoCache.clear();
-    QFileInfoList protosInfo;
-    findProtosRecursively(WbStandardPaths::projectsPath(), protosInfo);
-    gProjectsProtoCache << protosInfo;
+  gProjectsProtoCache.clear();
+  QFileInfoList protosInfo;
+  findProtosRecursively(WbStandardPaths::projectsPath(), protosInfo);
+  gProjectsProtoCache << protosInfo;
 }
 
 void WbProtoList::updateExtraProtoCache() {
-    gExtraProtoCache.clear();
-    QFileInfoList protosInfo;
-    if (!WbPreferences::instance()->value("General/extraProjectsPath").toString().isEmpty())
-        findProtosRecursively(WbPreferences::instance()->value("General/extraProjectsPath").toString(), protosInfo);
-    gExtraProtoCache << protosInfo;
+  gExtraProtoCache.clear();
+  QFileInfoList protosInfo;
+  if (!WbPreferences::instance()->value("General/extraProjectsPath").toString().isEmpty())
+    findProtosRecursively(WbPreferences::instance()->value("General/extraProjectsPath").toString(), protosInfo);
+  gExtraProtoCache << protosInfo;
 }
 
 void WbProtoList::updatePrimaryProtoCache() {
-    mPrimaryProtoCache.clear();
+  mPrimaryProtoCache.clear();
 
-    if (mPrimarySearchPath.isEmpty())
-        return;
+  if (mPrimarySearchPath.isEmpty())
+    return;
 
-    QFileInfoList protosInfo;
-    findProtosRecursively(mPrimarySearchPath, protosInfo, mPrimarySearchPath.endsWith("protos"));
-    mPrimaryProtoCache << protosInfo;
+  QFileInfoList protosInfo;
+  findProtosRecursively(mPrimarySearchPath, protosInfo, mPrimarySearchPath.endsWith("protos"));
+  mPrimaryProtoCache << protosInfo;
 }
 
-WbProtoModel* WbProtoList::readModel(const QString& fileName,
-                                     const QString& worldPath,
-                                     QStringList baseTypeList) const {
-    WbTokenizer tokenizer;
-    int errors = tokenizer.tokenize(fileName);
-    if (errors > 0)
+WbProtoModel *WbProtoList::readModel(const QString &fileName, const QString &worldPath, QStringList baseTypeList) const {
+  WbTokenizer tokenizer;
+  int errors = tokenizer.tokenize(fileName);
+  if (errors > 0)
+    return NULL;
+
+  WbParser parser(&tokenizer);
+  if (!parser.parseProtoInterface(worldPath))
+    return NULL;
+
+  tokenizer.rewind();
+  bool prevInstantiateMode = WbNode::instantiateMode();
+  try {
+    WbNode::setInstantiateMode(false);
+    WbProtoModel *model = new WbProtoModel(&tokenizer, worldPath, fileName, baseTypeList);
+    WbNode::setInstantiateMode(prevInstantiateMode);
+    return model;
+  } catch (...) {
+    WbNode::setInstantiateMode(prevInstantiateMode);
+    return NULL;
+  }
+}
+
+void WbProtoList::readModel(WbTokenizer *tokenizer, const QString &worldPath) {
+  WbProtoModel *model = NULL;
+  bool prevInstantiateMode = WbNode::instantiateMode();
+  try {
+    WbNode::setInstantiateMode(false);
+    model = new WbProtoModel(tokenizer, worldPath);
+    WbNode::setInstantiateMode(prevInstantiateMode);
+  } catch (...) {
+    WbNode::setInstantiateMode(prevInstantiateMode);
+    return;
+  }
+  mModels.prepend(model);
+  model->ref();
+}
+
+WbProtoModel *WbProtoList::findModel(const QString &modelName, const QString &worldPath, QStringList baseTypeList) {
+  // see if model is already loaded
+  foreach (WbProtoModel *model, mModels)
+    if (model->name() == modelName)
+      return model;
+
+  QFileInfoList availableProtoFiles;
+  availableProtoFiles << mPrimaryProtoCache << gExtraProtoCache << gProjectsProtoCache << gResourcesProtoCache;
+
+  foreach (const QFileInfo &fi, availableProtoFiles) {
+    if (fi.baseName() == modelName) {
+      WbProtoModel *model = readModel(fi.absoluteFilePath(), worldPath, baseTypeList);
+      if (model == NULL)  // can occur if the PROTO contains errors
         return NULL;
-
-    WbParser parser(&tokenizer);
-    if (!parser.parseProtoInterface(worldPath))
-        return NULL;
-
-    tokenizer.rewind();
-    bool prevInstantiateMode = WbNode::instantiateMode();
-    try {
-        WbNode::setInstantiateMode(false);
-        WbProtoModel* model = new WbProtoModel(&tokenizer, worldPath, fileName, baseTypeList);
-        WbNode::setInstantiateMode(prevInstantiateMode);
-        return model;
+      mModels << model;
+      model->ref();
+      return model;
     }
-    catch (...) {
-        WbNode::setInstantiateMode(prevInstantiateMode);
-        return NULL;
-    }
+  }
+
+  return NULL;  // not found
 }
 
-void WbProtoList::readModel(WbTokenizer* tokenizer, const QString& worldPath) {
-    WbProtoModel* model      = NULL;
-    bool prevInstantiateMode = WbNode::instantiateMode();
-    try {
-        WbNode::setInstantiateMode(false);
-        model = new WbProtoModel(tokenizer, worldPath);
-        WbNode::setInstantiateMode(prevInstantiateMode);
-    }
-    catch (...) {
-        WbNode::setInstantiateMode(prevInstantiateMode);
-        return;
-    }
-    mModels.prepend(model);
-    model->ref();
-}
+QString WbProtoList::findModelPath(const QString &modelName) const {
+  QFileInfoList availableProtoFiles;
+  availableProtoFiles << mPrimaryProtoCache << gExtraProtoCache << gProjectsProtoCache << gResourcesProtoCache;
 
-WbProtoModel* WbProtoList::findModel(const QString& modelName, const QString& worldPath, QStringList baseTypeList) {
-    // see if model is already loaded
-    foreach (WbProtoModel* model, mModels)
-        if (model->name() == modelName)
-            return model;
+  foreach (const QFileInfo &fi, availableProtoFiles) {
+    if (fi.baseName() == modelName)
+      return fi.absoluteFilePath();
+  }
 
-    QFileInfoList availableProtoFiles;
-    availableProtoFiles << mPrimaryProtoCache << gExtraProtoCache << gProjectsProtoCache << gResourcesProtoCache;
-
-    foreach (const QFileInfo& fi, availableProtoFiles) {
-        if (fi.baseName() == modelName) {
-            WbProtoModel* model = readModel(fi.absoluteFilePath(), worldPath, baseTypeList);
-            if (model == NULL)  // can occur if the PROTO contains errors
-                return NULL;
-            mModels << model;
-            model->ref();
-            return model;
-        }
-    }
-
-    return NULL;  // not found
-}
-
-QString WbProtoList::findModelPath(const QString& modelName) const {
-    QFileInfoList availableProtoFiles;
-    availableProtoFiles << mPrimaryProtoCache << gExtraProtoCache << gProjectsProtoCache << gResourcesProtoCache;
-
-    foreach (const QFileInfo& fi, availableProtoFiles) {
-        if (fi.baseName() == modelName)
-            return fi.absoluteFilePath();
-    }
-
-    return QString();  // not found
+  return QString();  // not found
 }
 
 QStringList WbProtoList::fileList() {
-    QStringList list;
-    foreach (WbProtoModel* model, gCurrent->mModels)
-        list << model->fileName();
-    return list;
+  QStringList list;
+  foreach (WbProtoModel *model, gCurrent->mModels)
+    list << model->fileName();
+  return list;
 }
