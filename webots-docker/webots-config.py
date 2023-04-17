5bae41f71b4a285269c3073a91fc6b731c03eb90
Jesse Williamson <jnathanwilliamson@gmail.com>
2023-04-17 13:07:39 +1000

    Allow comma separated addresses in run command and add teams/ prefix

--- webots-docker/webots-config.py
+++ webots-docker/webots-config.py
@@ -1,17 +1,15 @@
 import json, os, sys
 
 TEAM_NAME = os.environ.get('TEAM_NAME')
-ROBOT_HOST = os.environ.get('ROBOT_HOST')
-
-print(type(ROBOT_HOST))
+ROBOT_HOSTS = os.environ.get('ROBOT_HOSTS')
 
 filename = sys.argv[1]
 
 with open(filename, 'r') as f:
     game_config = json.load(f)
 
-if TEAM_NAME: game_config['red']['config'] = TEAM_NAME + '.json'
-if ROBOT_HOST: game_config['red']['hosts'].append(ROBOT_HOST)
+if TEAM_NAME: game_config['red']['config'] = 'teams/' + TEAM_NAME + '.json'
+if ROBOT_HOSTS: game_config['red']['hosts'] += ROBOT_HOSTS.split(",")
 
 with open(filename, 'w') as f:
     json.dump(game_config, f, indent=2)
