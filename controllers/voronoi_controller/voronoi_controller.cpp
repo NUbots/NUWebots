cfb4ec714a2a1aaae67f89c8f0ee30ba0de33828
Jason Disher <Jason.Disher@uon.edu.au>
2023-04-21 15:50:38 +1000

    End of day

--- controllers/voronoi_controller/voronoi_controller.cpp
+++ controllers/voronoi_controller/voronoi_controller.cpp
@@ -23,10 +23,50 @@
 #include <cstdlib>
 #include <memory>
 #include <webots/Robot.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Supervisor.hpp>
+#include <yaml-cpp/yaml.h>
 
 #include "utility/tcp.hpp"
 
 using namespace utility::tcp;
+
+    // Randomly find robot positions that do not clash and return those positions
+    std::vector<std::array<double, 3>> find_robot_positions(const std::vector<webots::Node*>& robot_nodes,
+                                                            //std::uniform_real_distribution<> x_distrib,
+                                                            //std::uniform_real_distribution<> y_distrib,
+                                                            //std::mt19937 gen,
+                                                            const double min_distance) {
+        // Loop through every robot and find valid teleport locations
+        std::vector<std::array<double, 3>> positions = {};
+
+        for (const auto& robot : robot_nodes) {
+            // Flag for if robots have collided. Start by assuming no collision, and then update if there is any collision.
+            bool collision = false;
+            // new_pos will be a "Proposed location" for a robot to teleport to
+            std::array<double, 3> new_pos{};
+            do {
+                collision = false;
+                // Generate a new random location
+                new_pos[0] = x_distrib(gen);
+                new_pos[1] = y_distrib(gen);
+                new_pos[2] = robot->getField("height")->getSFFloat();
+
+                // Loop through the vector of existing proposed locations and see if the new one is going to
+                // collide with any of them
+                for (const auto& testPos : positions) {
+                    const double distance =
+                        std::sqrt(std::pow((new_pos[1] - testPos[1]), 2) + std::pow((new_pos[0] - testPos[0]), 2));
+                    collision = distance < min_distance ? true : collision;
+                }
+                // Loop until a proposed location has been found that doesn't clash with the existing ones
+            } while (collision);
+            // Finally add the proposed location in as a confirmed position
+            positions.emplace_back(new_pos);
+        }
+        return positions;
+
+    }
 
 // This is the main program of your controller.
 // It creates an instance of your Robot instance, launches its
@@ -41,6 +81,19 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <TCP PORT> <CONTROLLER_TIME_STEP>" << std::endl;
         return EXIT_FAILURE;
     }
+
+    // GET SUPERVISOR, ROBOTS AND TIME STEP
+    // Load def arguments of other robots in the field into a vector
+    std::vector<webots::Node*> robot_nodes;
+    // Only one supervisor instance can exist in a single controller. As such the same one will be used
+    // every time
+    webots::Supervisor supervisor = webots::Supervisor();
+    for (int i = 2; i < argc; i++) {
+        // Load the Node of the robot by def argument and add to vector
+        robot_nodes.emplace_back(supervisor.getFromDef(argv[i]));
+    }
+    // Get the time step of the current world.
+    // int time_step = int(supervisor.getBasicTimeStep());
 
     // Load in the TCP port number from the command line and convert to an int
     int server_port = 0;
