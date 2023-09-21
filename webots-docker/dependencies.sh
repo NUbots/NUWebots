5a27f90c59cb7d9af803fa84f20aa573716cc911
Jesse Williamson <jnathanwilliamson@gmail.com>
2023-09-21 14:08:31 +1000

    Copy scripts instead of downloading from repo, clean up user setup

--- webots-docker/dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y ant cmake-curses-gui libprotobuf-dev protobuf-compiler libeigen3-dev libyaml-cpp-dev ninja-build clang-tidy python3-dev libjpeg9-dev sudo vim nano
